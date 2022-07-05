f08aca2725bec0eae40ea9c9d4a6d40aa912d816
fuang66 <108710361+fuang66@users.noreply.github.com>
2022-07-05 11:58:06 +0800

    Add files via upload
    Demo by Falcon

---
+++ AddDropMemberLogging.js
@@ -0,0 +1,30 @@
+//Demo by Falcon 
+
+/*
+ * Copyright 2002 by Mortice Kern Systems Inc. All rights reserved.
+ */
+
+/*
+ * This script should be set on post event for addmember and dropmember. This
+ * script will write a log file to the log directory named addDropMemberLogging.log.
+ * In this file it will write the project name, the date/time and action that occured.
+ */
+
+/* Lookup all necessary beans */
+var environmentBean = bsf.lookupBean("siEnvironmentBean");
+var projectBean = bsf.lookupBean("siProjectBean");
+var userBean = bsf.lookupBean("siUserBean");
+var addEeventArgsBean = bsf.lookupBean("siAddMemberArgumentsBean");
+var dropEeventArgsBean = bsf.lookupBean("siDropMemberArgumentsBean");
+
+var out = new java.io.PrintWriter(new java.io.FileWriter("../log/addDropMemberLogging.log",true));
+out.println("Time: " + java.util.Date());
+out.println("Project: " + projectBean.getDisplayName());
+out.println("User: " + userBean.getUsername());
+out.println("Event: " + environmentBean.getEventName());
+if( addEeventArgsBean != null ) {
+	out.println("Member: " + addEeventArgsBean.getMember());
+} else {
+	out.println("Member: " + dropEeventArgsBean.getMemberName());
+}
+out.close();
