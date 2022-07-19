88006576495063320197505dfdf9e06290789361
fuang66 <fuang66@gmail.com>
2022-07-19 13:31:44 +0800

    Add files via upload

---
+++ ConfigVol.cpp
@@ -0,0 +1,34 @@
+FI_INT CHNConfigManager::GetVolumeValue()
+{
+	return m_pConfigManager->GetVolumeValue();
+}
+
+FI_VOID CHNConfigManager::TurnUpVolume()
+{
+	m_pConfigManager->TurnUpVolume();
+}
+
+FI_VOID CHNConfigManager::TurnDownVolume()
+{
+	m_pConfigManager->TurnDownVolume();
+}
+
+FI_INT CHNConfigManager::GetVolumeLevelCount()
+{
+	return m_pConfigManager->GetVolumeLevelCount();
+}
+
+FI_VOID CHNConfigManager::SetVolumeLevelAt(FI_INT nIndex)
+{
+	m_pConfigManager->SetVolumeLevelAt(nIndex);
+}
+
+FI_VOID CHNConfigManager::SetVolumeMute(FI_BOOL bOn)
+{
+	m_pConfigManager->SetVolumeMute(bOn);
+}
+
+FI_BOOL CHNConfigManager::IsVolumeMuteOn()
+{
+	return m_pConfigManager->IsVolumeMuteOn();
+}
---
+++ ConfigVol.h
@@ -0,0 +1,20 @@
+class CHNConfigManager// : public fiTimerPerformer
+{
+  public:
+  	CHNConfigManager();
+  	virtual ~CHNConfigManager();
+
+	/////////////////////////////////////////////////////////////////////////////////
+	// Volume:
+	/////////////////////////////////////////////////////////////////////////////////
+	FI_INT GetVolumeValue();
+	FI_VOID TurnUpVolume();
+	FI_VOID TurnDownVolume();
+	FI_INT GetVolumeLevelCount();
+	FI_VOID SetVolumeLevelAt(FI_INT nIndex);
+	FI_VOID SetVolumeMute(FI_BOOL bOn);
+	FI_BOOL IsVolumeMuteOn();
+  protected:
+  private:
+	CConfigManager *m_pConfigManager;
+};
